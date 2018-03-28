7a2830e2328b81737dac854e92f33e876ec90848
Hana Ben Hadj Abdallah <h.benhadj@root.sa>
2018-03-28 14:08:13 +0100

    Add project files.

---
+++ CRUD-SECOND-VERSION/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUD_SECOND_VERSION.Startup))]
+namespace CRUD_SECOND_VERSION
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
